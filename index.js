2276cbd696b7dbb26319ce5ebfabf7072e4c93f0
Bawan Majid <bawanmajid23@gmail.com>
2020-06-16 13:30:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 class Formatter {
   //add static methods here
-  static capitalize(){}
-  static sanitize(){}
-  static titleize(){}
+  static capitalize(str){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+  
+  static titleize(string){
+    let words = string.split(' ');
+    let except = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    words[0] = this.capitalize(words[0]);
+    for (let i = 1; i < words.length; i++) {
+      if(!except.find(e => e == words[i])){
+         words[i] = this.capitalize(words[i]);
+      }
+    }
+    
+    return words.join(' ');
+}
 }
