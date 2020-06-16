871bab661e40ccaa061c7ec4811d7760ae38000f
Bawan Majid <bawanmajid23@gmail.com>
2020-06-16 13:55:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ class Formatter {
   
   static titleize(str){
     let word = str.split(' ');
-    let except = ['this','these','those','the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from','in','on']
+    let except = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
     
     word[0] = this.capitalize(word[0]);
     for (let w = 1; w < word.length; w++) {
